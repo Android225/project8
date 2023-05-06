634825944c4d8f05d5383ec5247cd95b74794b25
Student <ayd08151@zslsz.com>
2023-05-06 12:56:07 +0300

    Fix repos1

--- app/src/main/java/com/example/project8/UI/ViewModels/ClientVM.java
+++ app/src/main/java/com/example/project8/UI/ViewModels/ClientVM.java
@@ -15,12 +15,12 @@ import java.util.List;
 
 public class ClientVM extends AndroidViewModel {
     private final LiveData<List<Client>> mAllClient;
-    private final com.example.a8.data.ClientDB.ClientRepository mRepository;
+    private final com.example.project8.data.DataSources.Room.ClientRepository mRepository;
 
     public ClientVM(Application application) {
         super(application);
         Log.d("ClientVM", "Constructor");
-        mRepository = new com.example.a8.data.ClientDB.ClientRepository(application);
+        mRepository = new com.example.project8.data.DataSources.Room.ClientRepository(application);
         mAllClient = mRepository.getAllApartment();
     }
 
